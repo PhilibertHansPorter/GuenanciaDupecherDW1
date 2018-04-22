c05a38a07771c0dc6b1d7a4519c8692ce5cacd19
PhilibertHansPorter <33629870+PhilibertHansPorter@users.noreply.github.com>
2018-04-22 23:30:30 +0200

    cryptage working

--- login.php
+++ login.php
@@ -1,36 +1,27 @@
 <?php session_start();
-
 /********************************
  * DATABASE & FUNCTIONS
  ********************************/
 require('config/config.php');
 require('model/functions.fn.php');
-
-
 /********************************
  * PROCESS
  ********************************/
-
 if (isset($_POST['email']) && isset($_POST['password'])) {
     if (!empty($_POST['email']) && !empty($_POST['password'])) {
-
         $email = htmlspecialchars($_POST['email']);
         $password = htmlspecialchars($_POST['password']);
-
         // Force user connection to access dashboard
         userConnection($db, $email, $password);
-
         if (userConnection($db, $email, $password)) {
             header('Location: dashboard.php');
         } else {
             $error = "Mauvais identifiants";
         }
-
     } else {
         $error = 'Champs requis !';
     }
 }
-
 /********************************
  * VIEW
  ********************************/
--- register.php
+++ register.php
@@ -1,28 +1,11 @@
-<?php
+<?php session_start();
 require('config/config.php');
 require('model/functions.fn.php');
-session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) &&
-    !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+/*===============================
+	Register
+===============================*/
 
-    $username = htmlspecialchars($_POST['username']);
-    $email = htmlspecialchars($_POST['email']);
-    $password = htmlspecialchars($_POST['password']);
-    $passwordEncrypted = password_hash($password, PASSWORD_DEFAULT);
-
-    if(isUsernameAvailable($db, $username)) {
-        if(isEmailAvailable($db, $email)) {
-            userRegistration($db, $username, $email, $passwordEncrypted);
-        } else {
-            $error = "Email indisponible";
-        }
-    } else {
-        $error = "Username indisponible";
-    }
-
-
-}else{
-    $_SESSION['message'] = 'Erreur : Formulaire incomplet';
-    header('Location: register.php');
-}
+include 'view/_header.php';
+include 'view/register.php';
+include 'view/_footer.php';
