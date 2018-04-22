734bd1059e714b05e373df76ecebd22b1bfc714f
PhilibertHansPorter <33629870+PhilibertHansPorter@users.noreply.github.com>
2018-04-22 17:00:39 +0200

    register

--- register.php
+++ register.php
@@ -1,11 +1,28 @@
-<?php session_start();
+<?php
 require('config/config.php');
 require('model/functions.fn.php');
+session_start();
 
-/*===============================
-	Register
-===============================*/
+if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) &&
+    !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-include 'view/_header.php';
-include 'view/register.php';
-include 'view/_footer.php';
+    $username = htmlspecialchars($_POST['username']);
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+    $passwordEncrypted = password_hash($password, PASSWORD_DEFAULT);
+
+    if(isUsernameAvailable($db, $username)) {
+        if(isEmailAvailable($db, $email)) {
+            userRegistration($db, $username, $email, $passwordEncrypted);
+        } else {
+            $error = "Email indisponible";
+        }
+    } else {
+        $error = "Username indisponible";
+    }
+
+
+}else{
+    $_SESSION['message'] = 'Erreur : Formulaire incomplet';
+    header('Location: register.php');
+}
